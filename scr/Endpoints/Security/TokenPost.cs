adb52b90b2d45d59fe1e22c622b70935df575eff
Lênin Andrade <leninandrade.44@hotmail.com>
2023-01-31 20:51:35 -0300

    Configurando a expiração dos Tokens

--- scr/Endpoints/Security/TokenPost.cs
+++ scr/Endpoints/Security/TokenPost.cs
@@ -33,8 +33,8 @@ public class TokenPost
             SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature),
 
             Audience = configuration["JwtBearerTokenSettings:Audience"],
-            Issuer = configuration["JwtBearerTokenSettings:Issuer"]          
-            
+            Issuer = configuration["JwtBearerTokenSettings:Issuer"],
+            Expires = DateTime.UtcNow.AddMinutes(20)
         };
 
         var tokenHandler = new JwtSecurityTokenHandler();
